2b5cd526884f381a59969e815048c48aa081a6ae
breno-bortot <brenobortot@gmail.com>
2021-05-11 05:50:58 +0200

    deploy fanpage

---
+++ src/components/cv.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Curriculum = () => {
+    return (
+        <article className="cv">
+  
+  
+        <iframe title='Curriculum' style={{width: "95%", height:"96%", margin: "2%" }} src="Assets/cv.pdf" frameBorder="0"></iframe>
+    
+      
+     </article>
+    );
+};
+
+export default Curriculum;
---
+++ src/components/media.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Media = () => {
+    return (
+        <div className="album-grid">
+
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/FDXfmU6M2tc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+                <h1>Cicucia</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis officia totam ipsum veritatis sunt molestias necessitatibus saepe magni, labore nihil esse corrupti impedit? Incidunt sed unde excepturi aperiam possimus nulla! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias temporibus ducimus modi tempora. Ea totam excepturi porro a molestiae cum, soluta optio numquam unde beatae, molestias eius eaque,
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, voluptatem ipsum veniam nobis sequi veritatis inventore, necessitatibus saepe excepturi incidunt illum voluptate unde molestias vero modi doloribus libero fugiat. Culpa, ipsum deserunt consequuntur at quis voluptatibus porro voluptas suscipit, voluptates optio eius eaque velit minus voluptate pariatur tempora officia dolorum! Quis ratione aut, molestias enim temporibus debitis sapiente quo magni, corporis cupiditate autem tempora iste repellendus. Cum esse provident rerum, assumenda aliquid possimus dicta dolore tenetur, omnis vel laboriosam! Corrupti, hic! Voluptates ut asperiores rerum, blanditiis quo sed a itaque voluptatibus ad facere molestias odit placeat soluta incidunt aut, dolor et fuga non fugiat quidem odio corrupti! Modi illum iusto dolore adipisci temporibus odit rerum maiores? Libero ven</p>
+            </div>
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/_sbkyFviFAI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>ciaca</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias iusto distinctio quaerat vel, modi neque architecto culpa aliquam repudiandae molestiae quasi, odit, perferendis minus laudantium ex beatae? Aspernatur magni enim quibusdam non rerum quod placeat suscipit molestiae odit voluptates deleniti neque expedita eum necessitatibus nobis doloribus error mollitia, beatae repellendus. Qui eos et eum ea fugit dolore vero consectetur repellat?</p>
+            </div>
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/Jp7mE87I4uQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>asdasdas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure commodi inventore blanditiis enim! Voluptate culpa cupiditate cum aperiam adipisci omnis impedit. Quas, consectetur!</p>
+            </div>
+
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/9VwO47cU3lw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>asdasdas</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam pariatur at sed voluptatem sunt iure, perspiciatis est repellat perferendis sapiente nihil fugiat consequuntur accusantium? Eum iure, fugiat laboriosam et ducimus veniam aliquam distinctio illo possimus enim assumenda temporibus, maxime pariatur modi veritatis aliquid iusto repellat, ratione quasi! Sed atque dolor ab recusandae expedita, pariatur culpa deleniti aperiam iusto voluptatum quis odit consequatur maiores eaque nemo adipisci vitae esse, nesciunt temporibus maxime quidem debitis! Eos cum unde repellendus dolorum. Deleniti odit voluptas iusto at veritatis similique reprehenderit quos, hic tempora aliquid illum natus, ab eveniet ipsum minus minima dignissimos?</p>
+            </div>
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/t_iGLaXqQuU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>323</h1>
+                <p>euHaaiseaeisuseas</p>
+            </div>
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/Dkn7gKc-JbU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>232312312</h1>
+                <p>ehaisUheiausheasheuiashease</p>
+            </div>
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/B4tiEcmhdbg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>hasehasa</h1>
+                <p>huaisehiaseuihasehuihasieihauiseas</p>
+            </div>
+
+            <div className="album-grid__video">
+
+                <iframe src="https://www.youtube.com/embed/gWjUGRLsJ0M" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h1>ahsuehiauseis</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo sint dolores iusto dolor voluptatibus dicta totam. Omnis quaerat nostrum ut excepturi porro nulla cum debitis consequatur asperiores adipisci odit at consectetur, earum reiciendis suscipit obcaecati dolorem ad. Delectus nobis aliquid nam dolore, officia quis velit consequuntur repudiandae reiciendis eaque mollitia quam tempore tenetur sit ducimus doloremque suscipit tempora recusandae odit explicabo, esse facere ipsum molestias! Omnis consectetur blanditiis quas, voluptates hic in quod dolorem facilis aliquam saepe eius temporibus cum dicta reprehenderit, ea ipsa. Velit, optio.</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Media;
